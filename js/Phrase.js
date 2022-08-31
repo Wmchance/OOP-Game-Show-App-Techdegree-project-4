5db65e5b086a8c2fa7eaad207541a2035c423475
Will <wmchance@gmail.com>
2022-08-31 14:48:31 +0700

    'addPhraseToDisplay()' method added to Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -7,4 +7,25 @@ class Phrase {
         this.phrase = phrase.toLowerCase(); 
     }
 
+    /**
+    * Display phrase on game board
+    */
+    addPhraseToDisplay() {
+        const phraseDivUl = document.getElementById('phrase').firstElementChild;
+        
+        for(let i=0; i<this.phrase.length; i++) {
+            if(this.phrase[i] === ' ') {
+                let newSpaceLi = document.createElement('li');
+                newSpaceLi.classList.add('space'); 
+                newSpaceLi.innerText = ' ';
+                phraseDivUl.appendChild(newSpaceLi);
+            } else {
+                let newLetterLi = document.createElement('li');
+                newLetterLi.classList.add('hide', 'letter', `${this.phrase[i]}`); 
+                newLetterLi.innerText = `${this.phrase[i]}`;
+                phraseDivUl.appendChild(newLetterLi);
+            }
+        }
+    }
+
 }
--- js/app.js
+++ js/app.js
@@ -2,17 +2,20 @@
  * Project 4 - OOP Game App
  * app.js */
 
+const game = new Game();
+game.getRandomPhrase().addPhraseToDisplay();
+
+// const logPhrase = (phrase) => {
+//     console.log(`Phrase - phrase: `, phrase.phrase);
+//     };
+//     const game = new Game();
+//     logPhrase(game.getRandomPhrase());
+//     logPhrase(game.getRandomPhrase());
+//     logPhrase(game.getRandomPhrase());
+//     logPhrase(game.getRandomPhrase());
+//     logPhrase(game.getRandomPhrase());
+
 // const game = new Game();
 // game.phrases.forEach((phrase, index) => {
 // console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
 // });
-
-const logPhrase = (phrase) => {
-    console.log(`Phrase - phrase: `, phrase.phrase);
-    };
-    const game = new Game();
-    logPhrase(game.getRandomPhrase());
-    logPhrase(game.getRandomPhrase());
-    logPhrase(game.getRandomPhrase());
-    logPhrase(game.getRandomPhrase());
-    logPhrase(game.getRandomPhrase());
